36d3d7442dfde961ef4475a2443e0d0f852f9863
Jiawei LI <joopererer@gmail.com>
2024-04-12 23:33:04 +0200

    Test docker publish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import dataframe4j.DataFrame;
+import dataframe4j.IDataFrame;
 
+/**
+ * un demo pour Docker
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, ici est un demo de DataFrame4J.");
-
+        System.out.println("Hello, ici est un demo des utilisations sur DataFrame4J :");
+        System.out.println(">>");
+        System.out.println("#1, creer via un array et afficher tout");
         DataFrame df = new DataFrame(
                 new Object[][]{
                         {"Kate", "Sam", "Tom", "Fenny"},
@@ -14,6 +19,25 @@ public class Main {
                 new String[]{"name", "age", "gen"} // columns
         );
         df.print();
+
+        System.out.println("\n#2, afficher les deux premières lignes");
+        df.printHead(2);
+
+        System.out.println("\n#3, afficher les deux dernières lignes");
+        df.printTail(2);
+
+        System.out.println("\n#4, selection les lignes paires (via IndexFilter)");
+        DataFrame newDf = df.selectRows(new IDataFrame.IndexFilter() {
+            @Override
+            public boolean filter(int index) {
+                return index%2==0;
+            }
+        });
+        newDf.print();
+
+//        System.out.println("\n#5, calculer la moyenne sur age");
+//        float moyenne = df.mean("age");
+//        System.out.println("age moyenne:"+moyenne);
     }
 
 }
